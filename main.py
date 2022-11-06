91f9579a161dfde9a0de954220be71d3f8a4353b
ckruf <christian.kruf@gmail.com>
2022-11-06 16:39:23 +0100

    refactor

--- main.py
+++ main.py
@@ -1,34 +1,61 @@
 from __future__ import unicode_literals
-from typing import List, Any
+from typing import List, Optional
 import youtube_dl
 
-
 from selenium import webdriver
-import pandas as pd
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webelement import WebElement
 
-keyword = "short+sample+video"
-driver = webdriver.Chrome()
-driver.get("https://www.youtube.com/results?search_query=" + keyword)
-# find url by xpath and put into links list
-user_data = driver.find_elements("xpath", '//*[@id="video-title"]')
-links: List[Any] = []
-for i in user_data:
-    links.append(i.get_attribute('href'))
-# print links list
-for x in range(len(links)):
-    print(links[x])
-# get rid of the first None object in the list
-links.pop(0)
+
+keyword = "pure water"
+
+
+def main():
+    links = get_links(keyword)
+    download_videos(links)
+
+
+def get_links(query: str) -> List[str]:
+    """
+    Given a query, download all Youtube video results on the first page.
+
+    :param query: term to search on Youtube
+    :return: list of links to videos on first results page
+    """
+    keywords = query.split(" ")
+    url_params: Optional[str] = None
+    if len(keywords) > 1:
+        url_params = "+".join(keywords)
+    else:
+        url_params = query
+    driver = webdriver.Chrome()
+    driver.get("https://www.youtube.com/results?search_query=" + url_params)
+
+    # find url by xpath and put into links list
+    user_data: List[WebElement] = driver.find_elements("xpath", '//*[@id="video-title"]')
+    links: List[str] = []
+    for web_element in user_data:
+        links.append(web_element.get_attribute('href'))
+    # print links list
+    print(links)
+    # get rid of the first None object in the list
+    links.pop(0)
+    return links
+
 
 # method to download videos from list
-def download_videos(list):
-    ydl_opts = {}
-    for i in links:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([f'{i}'])
+def download_videos(video_links_list: List[str], download_options: dict = {}) -> None:
+    """
+    Given a list of youtube links, download the videos.
+
+    :param video_links_list: list of links to youtube videos
+    :param download_options: dict containing download options regarding video/sound quality
+    :return: None
+    """
+    # TODO give user option to specify folder to download to
+    for video_link in video_links_list:
+        with youtube_dl.YoutubeDL(download_options) as ydl:
+            ydl.download([f'{video_link}'])
 
 
-download_videos(links)
+if __name__ == "main":
+    main()
